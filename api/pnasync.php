3b2f981790816fba229bb6161d3a6e18aeeb860c
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2025-06-05 14:18:01 +0800

    Revert

--- api/pnasync.php
+++ api/pnasync.php
@@ -5,7 +5,8 @@ function getHTMLContent($url) {
     $opts = [
         "http" => [
             "method" => "GET",
-            "header" => "User-Agent: NetcraftSurveyAgent/1.0",
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
+                        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\n"
         ]
     ];
     $context = stream_context_create($opts);
@@ -14,7 +15,7 @@ function getHTMLContent($url) {
 }
 
 // Fetch the HTML content
-$url = 'https://www.pna.gov.ph/articles/list';
+$url = 'https://newsorgonmastodonproxy.vercel.app/pna-proxy';
 $html = getHTMLContent($url);
 
 if ($html === false) {
