6bf40bca07b3649865b906cccef1abd58538a053
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2025-06-05 13:52:34 +0800

    Hoping it works.

--- api/pna-proxy.js
+++ api/pna-proxy.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://www.pna.gov.ph/articles/list", {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+          NetcraftSurveyAgent/1.0",
       },
     });
 
