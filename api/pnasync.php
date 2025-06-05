741caf659472957f9552b7bd307917f77b6df946
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2025-06-05 13:33:05 +0800

    Update pnasync.php

--- api/pnasync.php
+++ api/pnasync.php
@@ -5,8 +5,7 @@ function getHTMLContent($url) {
     $opts = [
         "http" => [
             "method" => "GET",
-            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
-                        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\n"
+            "header" => "User-Agent: facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)"
         ]
     ];
     $context = stream_context_create($opts);
