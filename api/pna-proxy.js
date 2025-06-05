c5eb7e768f309bdb8e5e6a44dbfe64d9e78cf48d
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2025-06-05 13:30:07 +0800

    Attempt to fix

--- api/pna-proxy.js
+++ api/pna-proxy.js
@@ -3,8 +3,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://www.pna.gov.ph/articles/list", {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-          "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+          "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
       },
     });
 
